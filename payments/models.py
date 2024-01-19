4b7bd9cd7e3a57f3dd291567bf411a16e62ee72e
Mark_Dolgov <hontonikimoi@gmail.com>
2024-01-19 19:38:15 +0300

    update models and serializers

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def get_absolute_url(self):
+        return reverse("get_item", args=[self.pk])
