dc6530e919b3a6d43d2e53b23383c88aa97a1f77
Mark_Dolgov <hontonikimoi@gmail.com>
2024-01-21 19:29:22 +0300

    update views, urls

--- payments/urls.py
+++ payments/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('buy/<int:pk>/', views.create_checkout_session, name='create_checkout_session'),
+    path('buy/order/<int:pk>/', views.create_order_checkout_session, name='create_order_checkout_session'),
     path('item/<int:pk>/', views.get_item, name='get_item'),
+    path('order/<int:pk>/', views.get_order, name='get_order'),
     path('success/', views.SuccessView.as_view()),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,42 +1,45 @@
-import stripe
 from django.conf import settings
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.views import View
+from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_GET
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
-from .models import Item
+from . import services
+from .models import Item, Order
 
 
 @api_view()
 def create_checkout_session(request, pk):
-    item = get_object_or_404(Item, id=pk)
-    stripe.api_key = settings.STRIPE_SECRET_KEY
-
-    checkout_session = stripe.checkout.Session.create(
-        success_url=request.build_absolute_uri('/success'),
-        cancel_url=request.build_absolute_uri(item.get_absolute_url()),
-        mode='payment',
-        line_items=[
-            {
-                'price_data': {
-                    'currency': 'usd',
-                    'product_data': {
-                        'name': item.name,
-                        'description': item.description,
-                    },
-                    'unit_amount': int(item.price * 100),
-                },
-                'quantity': 1
-            }])
-
+    checkout_session = services.create_checkout_session(request, pk)
     return JsonResponse(checkout_session, status=200)
 
 
 @require_GET
 def get_item(request, pk):
     item = Item.objects.get(id=pk)
-    return render(request, 'payments/item.html', {'item': item, 'stripe_key': settings.STRIPE_PUBLISHABLE_KEY})
+    return render(request, 'payments/item.html', {
+        'item': item,
+        'stripe_key': settings.STRIPE_PUBLISHABLE_KEY,
+    })
+
+
+def create_order_checkout_session(request, pk):
+    checkout_session = services.create_order_checkout_session(request, pk)
+    return JsonResponse(checkout_session, status=200)
+
+
+@require_GET
+def get_order(request, pk):
+    order = get_object_or_404(Order, id=pk)
+    items = order.items.all()
+    total_price = sum(item.price for item in items)
+    return render(request, 'payments/order.html', {
+        'order': order,
+        'items': items,
+        'price': total_price,
+        'stripe_key': settings.STRIPE_PUBLISHABLE_KEY,
+    })
 
 
 class SuccessView(View):
