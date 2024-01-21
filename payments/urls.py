a4e387d0aed6769748270019dcfbf448a3831c27
Mark_Dolgov <hontonikimoi@gmail.com>
2024-01-21 15:26:17 +0300

    update views, urls

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
-
 urlpatterns = [
-    path('item/', views.ItemListView.as_view())
+    path('buy/<int:pk>/', views.create_checkout_session, name='create_checkout_session'),
+    path('item/<int:pk>/', views.get_item, name='get_item'),
+    path('success/', views.SuccessView.as_view()),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,9 +1,44 @@
-from rest_framework import generics
-
+import stripe
+from django.conf import settings
+from django.shortcuts import get_object_or_404, render
+from django.views import View
+from django.views.decorators.http import require_GET
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
 from .models import Item
-from .serializers import ItemSerializer
 
 
-class ItemListView(generics.ListAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
+@api_view()
+def create_checkout_session(request, pk):
+    item = get_object_or_404(Item, id=pk)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    checkout_session = stripe.checkout.Session.create(
+        success_url=request.build_absolute_uri('/success'),
+        cancel_url=request.build_absolute_uri(item.get_absolute_url()),
+        mode='payment',
+        line_items=[
+            {
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.name,
+                        'description': item.description,
+                    },
+                    'unit_amount': int(item.price * 100),
+                },
+                'quantity': 1
+            }])
+
+    return JsonResponse(checkout_session, status=200)
+
+
+@require_GET
+def get_item(request, pk):
+    item = Item.objects.get(id=pk)
+    return render(request, 'payments/item.html', {'item': item, 'stripe_key': settings.STRIPE_PUBLISHABLE_KEY})
+
+
+class SuccessView(View):
+    def get(self, request):
+        return render(request, 'payments/success.html', status=200)
